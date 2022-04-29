d05aaccf15dd0d7168baf8ff097a9e27362b7fc3
MikhailS89 <mihey2023@ya.ru>
2022-04-29 12:32:35 +0400

    add spiner and v-cloak

--- js/index.js
+++ js/index.js
@@ -15,19 +15,5 @@ const Note = {
             return this.counter = 0
         }
     },
-    // computed: {
-    //     subtractionDeg() {
-    //         console.log('subtractionDeg', this.counter - 30)
-    //         return this.counter - 30
-    //     },
-    //     addationDeg() {
-    //         console.log('addationDeg', this.counter + 30)
-    //         return this.counter + 30
-    //     },
-    //     resetDeg() {
-    //         console.log('resetDeg', this.counter = 0)
-    //         return this.counter = 0
-    //     }
-    // }
 }
 Vue.createApp(Note).mount('#note')
